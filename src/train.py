c6013e239dfdd4fe0509aa026ff178371735a806
kha <phamvankha910@gmail.com>
2026-01-20 00:34:46 +0100

    added lfs pulling

--- src/train.py
+++ src/train.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from xgboost import XGBClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import precision_recall_curve, auc
@@ -23,10 +22,6 @@ X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.2,stratify=y
 X_train_resampled, y_train_resampled=SMOTE(random_state=42).fit_resample(X_train, y_train)
 
 
-scaler=StandardScaler()
-X_train_resampled_scaler=scaler.fit_transform(X_train_resampled)
-X_test_scaler=scaler.transform(X_test)
-
 param_dist = {
     'n_estimators': randint(1000, 3001),
     'learning_rate': uniform(0.001, 0.199),
@@ -40,8 +35,6 @@ XGB_random = RandomizedSearchCV(estimator=XGB, param_distributions=param_dist, n
 XGB_random.fit(X_train_resampled, y_train_resampled)
 y_score=XGB_random.predict_proba(X_test)[:,1]
 precision, recall, _ = precision_recall_curve(y_test, y_score)
-print("Precision", precision)
-print("Recall", recall)
 print(auc(recall, precision))
 
 os.makedirs('models', exist_ok=True)
@@ -57,9 +50,7 @@ timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 new_metrics = pd.DataFrame({
     'timestamp': [timestamp],
-    'precision': [precision],
-    'recall': [recall],
-    'auc': [auc(recall, precision)]
+    'pr_auc': [auc(recall, precision)]
 })
 
 if os.path.exists(metrics_file):
